4cddeed102b29c886a88864ce68270a225238ef3
sabueso <sabueso@veriton>
2013-09-07 17:30:14 +0200

    Little corrections with ip like arguments

--- ublogin.py
+++ ublogin.py
@@ -16,9 +16,9 @@ print "===================================================================="
 datos = {'uri': '/', 'username': ''+sys.argv[2]+'','password':''+sys.argv[3]+''}
 #Encontrado en https://github.com/shazow/urllib3/issues/111
 ficheros = {'file': ('form.txt', open('form.txt', 'rb'))}
-a = requests.post('http://192.168.1.20/login.cgi', cookies=cookiesairos, data=datos, files=ficheros)
+a = requests.post('http://'+str(sys.argv[1])+'/login.cgi', cookies=cookiesairos, data=datos, files=ficheros)
 print "Status de autentificacion:"+str(a.status_code)+""
 print "===================================================================="
-b= requests.get('http://192.168.1.20/iflist.cgi',cookies=cookiesairos)
+b= requests.get('http://'+str(sys.argv[1])+'/iflist.cgi',cookies=cookiesairos)
 print "Salida final"
 print b.text
