ed92c3cbbd64dd0275ea7eaa055f64ae21092756
sabueso <sabueso@veriton>
2013-09-08 18:49:47 +0200

    Implement a little debug function

--- ublogin.py
+++ ublogin.py
@@ -10,7 +10,6 @@ except ImportError:
 else:
 	pass
 
-
 def airosauth(ipdata,usernamedata,passdata):
 	#Query for session and executes the auth request to the web server
 	r=requests.get('http://'+ipdata+'/login.cgi')
@@ -26,13 +25,29 @@ def airosauth(ipdata,usernamedata,passdata):
 	#Make the request, sending the cookies, data for auth, and form to do it well
 	a=requests.post('http://'+ipdata+'/login.cgi', cookies=cookiesairos, data=authdata, files=authformfile)
 	#Return status code of the web request
+	if debug=="on":
+		print "===="
+		print "Output of \"a\" object"
+		print "--"
+		print a
+		print "--"
+		print a.text
+		print "===="
 	return
 
 def datareq(ipdata):
 	#Do the request of the cgi scripts that retrieves the data
 	global b
 	b=requests.get('http://'+ipdata+'/iflist.cgi',cookies=cookiesairos)
-	#print b.text
+        if debug=="on":
+                print "===="
+                print "Output of \"b\" object"
+                print "--"
+                print b
+                print "--"
+                print b.text
+                print "===="
+        return
 
 def filterdata(req):
 	#Filter the neccesary data and clean it
@@ -51,15 +66,14 @@ def filterdata(req):
 				of=re.sub(" MHz\"",'',of).strip("\"")
 			print str(of)
 
-
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv,"hi:u:p:s:d:")
+		opts, args = getopt.getopt(argv,"hi:u:p:s:d:v")
 	except getopt.GetoptError:
 		print "Options are not recognized: -i ip/hostname -u user -p password -s ssl -d [channel|freq|ccq]"
 	for opt, arg in opts:
 		if opt == '-h':
-			print "Options are not recognized: -i ip/hostname -u user -p password -s [ssl|off] -d [channel|freq|ccq]"
+			print "Options are not recognized: -i ip/hostname -u user -p password -s [ssl|off] -d [channel|frequency|ccq]"
 			sys.exit()
 		elif opt in ("-i"):
 			ip = arg
@@ -71,9 +85,18 @@ def main(argv):
 			sec = arg
 		elif opt in ("-d"):
 			info = arg
+	#Control de verbose mode
+	if "-v" in str(opts):
+		global debug
+		debug="on"
+		print "Debugdata:"+debug+""
+		print opts
+	else:
+		debug="off"
+	#Execute the 3 functions of the script...
 	airosauth(ip,username,password)
 	datareq(ip)
 	filterdata(info)
 
 if __name__ == "__main__":
-	   main(sys.argv[1:])
+	main(sys.argv[1:])
